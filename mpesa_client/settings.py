94996575bc768ff8e5ef4658e1e6d0fdc240e415
david <davidmuraya@gmail.com>
2023-06-27 13:39:25 +0300

    initial commit

--- mpesa_client/settings.py
+++ mpesa_client/settings.py
@@ -1,21 +1,26 @@
 from pydantic import BaseModel
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 
 class MpesaSandboxSettings(BaseModel):
     """
     Please consider storing these values in a secure location e.g. firestore
+    populate a .env file with the following values:
+
     """
 
     sandbox_url: str = "https://sandbox.safaricom.co.ke/"
-    c2b_consumer_key: str = "wLs9wxNfPrda0fIy7MUjwWuRWW5xQ8N5"
-    c2b_consumer_secret: str = "hQowLbqAnrNvQVOI"
-    lipa_na_mpesa_consumer_key: str = "j0Nk3MmfUSiEpYSSavzXGSyO47pPeGZD"
-    lipa_na_mpesa_consumer_secret: str = "kOCIt9T5pJ919L8m"
-    lipa_na_mpesa_passkey: str = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-    c2b_confirmation_url = ""
-    c2b_validation_url = ""
-    lipa_na_mpesa_callback_url = "https://demo.bima-mo.com/app/receipts/payment-confirmation?confirmation_code=c"
-    lipa_na_mpesa_business_short_code = "174379"
+    c2b_consumer_key: str = config.get("C2B_CONSUMER_KEY")
+    c2b_consumer_secret: str = config.get("C2B_CONSUMER_SECRET")
+    lipa_na_mpesa_consumer_key: str = config.get("LIPA_NA_MPESA_CONSUMER_KEY")
+    lipa_na_mpesa_consumer_secret: str = config.get("LIPA_NA_MPESA_CONSUMER_SECRET")
+    lipa_na_mpesa_passkey: str = config.get("LIPA_NA_MPESA_PASSKEY")
+    c2b_confirmation_url: str = config.get("C2B_CONFIRMATION_URL")
+    c2b_validation_url: str = config.get("C2B_VALIDATION_URL")
+    lipa_na_mpesa_callback_url: str = config.get("LIPA_NA_MPESA_CALLBACK_URL")
+    lipa_na_mpesa_business_short_code: str = config.get("LIPA_NA_MPESA_BUSINESS_SHORT_CODE")
 
 
 class MpesaProductionSettings(BaseModel):
